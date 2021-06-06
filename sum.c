336c7d89ba399b298f0de129143cefa45b93fdca
amalkanhangad <83081466+amalkanhangad@users.noreply.github.com>
2021-06-06 21:07:04 +0530

    Add files via upload

---
+++ sum.c
@@ -0,0 +1,10 @@
+//PROGRAM TO FIND SUM OF TWO NUMBERS
+#include<stdio.h>
+void main()
+{
+  int a,b,s;
+  printf("Enter two numbers:\n");
+  scanf("%d%d\n",&a,&b );
+  s=a+b;
+  printf("%d" sum is ",s);
+}
